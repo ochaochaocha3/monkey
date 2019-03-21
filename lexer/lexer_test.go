5194013dc674bc3f2371d460232dd29a96c2354c
ocha <ochaochaocha3@gmail.com>
2019-03-22 00:32:35 +0900

    lexer: 追加のキーワードに対応させる

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -132,3 +132,37 @@ func TestNextTokenForAdditionalOperators(t *testing.T) {
 
 	doTestNextToken(input, tests, t)
 }
+
+func TestNextTokenForAdditionalKeywords(t *testing.T) {
+	input := `if (5 < 10) {
+	return true;
+} else {
+	return false;
+}
+`
+
+	tests := []tokenExpectation{
+		{token.IF, "if"},
+		{token.LPAREN, "("},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+
+		{token.RETURN, "return"},
+		{token.TRUE, "true"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+
+		{token.ELSE, "else"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.FALSE, "false"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.EOF, ""},
+	}
+
+	doTestNextToken(input, tests, t)
+}
--- token/token.go
+++ token/token.go
@@ -38,11 +38,21 @@ const (
 	// キーワード
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
 )
 
 var keywords = map[string]TokenType{
-	"fn":  FUNCTION,
-	"let": LET,
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
 }
 
 // keywordsテーブルをチェックし、渡された演算子がキーワードでないか確認する。
